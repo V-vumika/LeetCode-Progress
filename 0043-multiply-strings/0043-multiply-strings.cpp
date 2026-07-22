24b5710b3dff244fdac902af0ee89232245f067a
V-vumika <vumikabiswas40@gmail.com>
2026-07-22 21:50:28 +0530

    Time: 2 ms (66.07%), Space: 8.9 MB (97.75%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -3,28 +3,24 @@ public:
     string multiply(string num1, string num2) {
         if (num1 == "0" || num2 == "0") return "0";
 
-        int n = num1.size();
-        int m = num2.size();
+        int n = num1.size(), m = num2.size();
+        string ans(n + m, '0');
 
-        vector<int> result(n + m, 0);
+        for (int i = n - 1; i >= 0; --i) {
+            int carry = 0;
+            for (int j = m - 1; j >= 0; --j) {
+                int cur = (ans[i + j + 1] - '0') +
+                          (num1[i] - '0') * (num2[j] - '0') +
+                          carry;
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                int mul = (num1[i] - '0') * (num2[j] - '0');
-
-                int sum = mul + result[i + j + 1];
-
-                result[i + j + 1] = sum % 10;
-                result[i + j] += sum / 10;
+                ans[i + j + 1] = (cur % 10) + '0';
+                carry = cur / 10;
             }
+            ans[i] += carry;
         }
 
-        string ans = "";
-        for (int num : result) {
-            if (!(ans.empty() && num == 0))
-                ans += (num + '0');
-        }
-
-        return ans.empty() ? "0" : ans;
+        int i = 0;
+        while (i < ans.size() - 1 && ans[i] == '0') i++;
+        return ans.substr(i);
     }
 };
