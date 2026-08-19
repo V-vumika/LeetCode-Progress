f7cedaf1cc1e9e0aae728f203c2ca89bb881491b
Aparna Biswas <vumikabiswas40@gmail.com>
2026-08-19 12:13:58 +0530

    Time: 20 ms (77.53%), Space: 60.1 MB (73.48%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -3,7 +3,6 @@ public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         unordered_map<int, int> mp;
 
-        // Store reserved seats as a bitmask for each row
         for (auto &seat : reservedSeats) {
             int row = seat[0];
             int col = seat[1];
@@ -11,7 +10,6 @@ public:
             mp[row] |= (1 << col);
         }
 
-        // Initially every row can accommodate 2 groups
         int ans = 2 * n;
 
         for (auto &[row, mask] : mp) {
@@ -31,7 +29,6 @@ public:
                 groups = 0;
             }
 
-            // This row was initially counted as 2
             ans -= (2 - groups);
         }
 
