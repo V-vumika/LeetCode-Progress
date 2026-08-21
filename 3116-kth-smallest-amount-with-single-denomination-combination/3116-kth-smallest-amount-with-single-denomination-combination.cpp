8a05c1b028d3e9f6562ba9f5b273b04dfeda8d74
Aparna Biswas <vumikabiswas40@gmail.com>
2026-08-21 14:29:13 +0530

    Time: 11 ms (81.4%), Space: 23.2 MB (5.43%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
@@ -6,31 +6,40 @@ public:
 
         int n = coins.size();
 
+        vector<pair<long long, int>> subsets;
+
+        // Precompute LCM and number of selected coins
+        for (int mask = 1; mask < (1 << n); mask++) {
+            long long lcm = 1;
+            int bits = 0;
+            bool ok = true;
+
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    bits++;
+
+                    long long g = gcd(lcm, (long long)coins[i]);
+                    lcm /= g;
+
+                    if (lcm > 2000000000000000000LL / coins[i]) {
+                        ok = false;
+                        break;
+                    }
+
+                    lcm *= coins[i];
+                }
+            }
+
+            if (ok)
+                subsets.push_back({lcm, bits});
+        }
+
+        // Count valid numbers <= x
         auto count = [&](long long x) {
             long long ans = 0;
 
-            for (int mask = 1; mask < (1 << n); mask++) {
-                long long lcm = 1;
-                int bits = 0;
-                bool ok = true;
-
-                for (int i = 0; i < n; i++) {
-                    if (mask & (1 << i)) {
-                        bits++;
-
-                        long long g = gcd(lcm, (long long)coins[i]);
-                        lcm /= g;
-
-                        if (lcm > x / coins[i]) {
-                            ok = false;
-                            break;
-                        }
-
-                        lcm *= coins[i];
-                    }
-                }
-
-                if (!ok || lcm > x)
+            for (auto [lcm, bits] : subsets) {
+                if (lcm > x)
                     continue;
 
                 long long cnt = x / lcm;
@@ -44,8 +53,9 @@ public:
             return ans;
         };
 
+        // Binary search
         long long low = 1;
-        long long high = 1LL * (*min_element(coins.begin(), coins.end())) * k;
+        long long high = 1LL * coins[0] * k;
 
         while (low < high) {
             long long mid = low + (high - low) / 2;
