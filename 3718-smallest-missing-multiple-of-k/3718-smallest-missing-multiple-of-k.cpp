8ae439f54012471fc9b4d699e9d40d0b06f569c5
Aparna Biswas <vumikabiswas40@gmail.com>
2026-08-25 12:08:07 +0530

    Time: 0 ms (100%), Space: 24.1 MB (82.33%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int> s(nums.begin(), nums.end());
+        bool present[101] = {};
 
-        int x = k;
-
-        while (s.count(x)) {
-            x += k;
+        for (int x : nums) {
+            present[x] = true;
         }
 
-        return x;
+        for (int multiple = k; ; multiple += k) {
+            if (multiple > 100 || !present[multiple]) {
+                return multiple;
+            }
+        }
     }
 };
