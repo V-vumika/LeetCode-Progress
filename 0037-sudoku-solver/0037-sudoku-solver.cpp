3f8f716cce1312266b698b2fcfaa9608c37461e7
Aparna Biswas <vumikabiswas40@gmail.com>
2026-09-16 20:08:21 +0530

    Time: 51 ms (96.97%), Space: 8.8 MB (60.75%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        // প্রতিটা row, col, box এর জন্য bitmask ইনিশিয়ালাইজ করা
+        int rows[9] = {0}, cols[9] = {0}, boxes[9] = {0};
+        
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] != '.') {
+                    int bit = 1 << (board[r][c] - '0');
+                    rows[r] |= bit;
+                    cols[c] |= bit;
+                    boxes[(r / 3) * 3 + (c / 3)] |= bit;
+                }
+            }
+        }
+        
+        backtrack(board, rows, cols, boxes, 0);
+    }
+    
+private:
+    bool backtrack(vector<vector<char>>& board, int rows[], int cols[], int boxes[], int pos) {
+        if (pos == 81) return true; // সব সেল ভরা হয়ে গেছে
+        
+        int r = pos / 9;
+        int c = pos % 9;
+        
+        if (board[r][c] != '.') {
+            return backtrack(board, rows, cols, boxes, pos + 1); // আগে থেকে ভরা সেল skip
+        }
+        
+        int b = (r / 3) * 3 + (c / 3);
+        
+        for (int d = 1; d <= 9; d++) {
+            int bit = 1 << d;
+            
+            if ((rows[r] & bit) || (cols[c] & bit) || (boxes[b] & bit)) continue;
+            
+            board[r][c] = '0' + d;
+            rows[r] |= bit;
+            cols[c] |= bit;
+            boxes[b] |= bit;
+            
+            if (backtrack(board, rows, cols, boxes, pos + 1)) return true;
+            
+            board[r][c] = '.';
+            rows[r] &= ~bit;
+            cols[c] &= ~bit;
+            boxes[b] &= ~bit;
+        }
+        
+        return false;
+    }
+};
